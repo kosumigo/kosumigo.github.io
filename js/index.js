cf3d016c1c31900fad95a779e9c1a2a16c457268
Sander Vonk <sander.c.vonk@gmail.com>
2022-07-22 21:33:43 -0700

    tweaks

--- js/index.js
+++ js/index.js
@@ -9,3 +9,10 @@ $("#sections-nav > li").click(function () {
 $(".footer-item").click(function () {
   window.location.href = $(this).attr("href");
 });
+$.getJSON("../data/meetings.json")
+  .then((data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.error("Error getting meetings JSON:", error.error);
+  });
