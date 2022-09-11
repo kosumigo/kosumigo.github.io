164dda78531cdfa1e2420cd9b2ab5f1840bddb3d
Sander Vonk <sander.c.vonk@gmail.com>
2022-09-10 19:36:32 -0700

    tweaks and add icons
    - start building auth
    - add in util script (toasts, popup, etc) + common styles from `sandervonk/VITE`

---
+++ js/auth.js
@@ -0,0 +1,39 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+import { getFirestore, collection, addDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyA9HsjfAhwcTsRgoO65nUy0dAGOnBv1he4",
+  authDomain: "kosumigo.firebaseapp.com",
+  projectId: "kosumigo",
+  storageBucket: "kosumigo.appspot.com",
+  messagingSenderId: "837493583764",
+  appId: "1:837493583764:web:34da4cd5541b3bae512981",
+  measurementId: "G-WRXBCFQ1SH",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const analytics = getAnalytics(app);
+
+// setup firebase authentication
+auth.onAuthStateChanged((user) => {
+  if (user) {
+    // User is signed in, see docs for a list of available properties
+    // https://firebase.google.com/docs/reference/js/firebase.User
+    // var uid = user.uid;
+    // ...
+  } else {
+    // User is signed out
+    // ...
+    new Toast("Sorry, you need to be logged in to access the dashboard", "default", 5000, "/img/icon/toast/error-icon.svg", "/");
+  }
+});
---
+++ js/util.js
@@ -0,0 +1,108 @@
+"use strict";
+/** URI SEARCH TERMS **/
+var params = new URLSearchParams(window.location.search);
+if (window.history) {
+  history.replaceState({}, "", window.location.href.substr(0, window.location.href.length - window.location.search.length));
+}
+
+/** TOAST **/
+
+class Toast {
+  constructor(message, type, duration, iconPath = "", action = "") {
+    this.message = message;
+    this.type = type;
+    this.duration = duration;
+    this.icon = iconPath;
+    this.action = action;
+    this.showToast();
+  }
+  showToast() {
+    $(".toast").remove();
+    let overlay = document.createElement("div"),
+      toast = document.createElement("div");
+    toast.classList.add("toast");
+    overlay.classList.add("toast-overlay");
+    toast.classList.add(this.type);
+    if (this.icon != "") {
+      toast.innerHTML += `<img src="${this.icon}" class="toast-icon" alt="Toast Popup Icon">`;
+    }
+    toast.innerHTML += this.message;
+    if (this.action != "") {
+      document.body.appendChild(overlay);
+    }
+    document.body.appendChild(toast);
+    setTimeout(() => {
+      $(toast).css({ animation: "slideOut 0.5s forwards" });
+    }, this.duration);
+    setTimeout(() => {
+      overlay.remove();
+      toast.remove();
+      if (this.action != "" && this.action != "") {
+        window.location.href = this.action;
+      }
+    }, this.duration + 500);
+  }
+}
+class ErrorToast extends Toast {
+  constructor(message, err, duration, action = "") {
+    message += ": " + err;
+    super(message, "default", duration, "/img/icon/error-icon.svg", action);
+  }
+}
+/** POPUP **/
+
+class Popup {
+  constructor(message, type, duration, iconPath = "", action = "") {
+    this.message = message;
+    this.type = type;
+    this.duration = duration;
+    this.icon = iconPath;
+    this.action = action;
+    this.showPopup();
+  }
+  showPopup() {
+    $(".popup, .popup-overlay").remove();
+    let overlay = document.createElement("div"),
+      toast = document.createElement("div"),
+      buttons = "<div id='popup-buttons'>";
+    toast.classList.add("popup");
+    overlay.classList.add("popup-overlay");
+    for (let classData of this.type.split(" ")) {
+      toast.classList.add(classData);
+    }
+    for (let actionInfo of this.action) {
+      buttons += `<button class="popup-button ${actionInfo[2] == undefined ? "" : " " + actionInfo[2]}" onclick="${actionInfo[0]}">${actionInfo[1]}</button>`;
+    }
+    buttons += "</div>";
+    toast.innerHTML += (this.icon != "" ? `<div class="popup-top-bar">` : "") + "<div class='popup-text'>" + this.message + `</div>` + (this.icon != "" ? `<img src="${this.icon}" class="popup-icon" alt="Popup Icon"></div>` : "");
+    toast.innerHTML += buttons;
+    if (this.action != "") {
+      document.body.appendChild(overlay);
+    }
+    document.body.appendChild(toast);
+    setTimeout(() => {
+      $(toast).css({ animation: "fadeout 0.5s forwards" });
+      $(overlay).css({ animation: "fadeout 0.5s forwards" });
+    }, this.duration);
+    setTimeout(() => {
+      $(toast).remove();
+      $(overlay).remove();
+    }, this.duration + 500);
+  }
+}
+function removePopup() {
+  $(".popup").css({ animation: "fadeout 0.5s forwards" });
+  $(".popup-overlay").css({ animation: "fadeout 0.5s forwards" });
+  setTimeout(() => {
+    $(".popup").remove();
+    $(".popup-overlay").remove();
+  }, 500);
+}
+$(document.body).on("click", ".popup-overlay", function () {
+  removePopup();
+});
+
+/** Other **/
+$("[placeholdaction]").click(function () {
+  new Toast("This feature hasn't been implemented yet, sorry! 🤫", "default", 1500, "/img/icon/unimplemented-icon.svg");
+});
