a917039ee165abf7307f6dd78539f7edd7363347
Sander Vonk <sander.c.vonk@gmail.com>
2022-11-23 17:31:34 -0800

    setup password reset

--- js/auth.js
+++ js/auth.js
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
 import { getFirestore, doc, collection, addDoc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js";
-import { getAuth, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, sendEmailVerification, RecaptchaVerifier } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
+import { getAuth, signOut, createUserWithEmailAndPassword, signInWithEmailAndPassword, onAuthStateChanged, sendEmailVerification, sendPasswordResetEmail, verifyPasswordResetCode, confirmPasswordReset } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-analytics.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -48,7 +48,7 @@ onAuthStateChanged(auth, (user) => {
           new ErrorToast("Could not send verification email", cleanError(error), 5000);
         });
     }
-  } else if (window.location.pathname != "/dashboard/login.html" && window.location.pathname != "/dashboard/signup.html" && window.location.pathname != "/dashboard/reset-password.html") {
+  } else if (window.location.pathname != "/dashboard/login.html" && window.location.pathname != "/dashboard/signup.html" && window.location.pathname != "/dashboard/forgot-password.html" && window.location.pathname != "/dashboard/reset.html") {
     new Toast("Sorry, you must be logged in to access this page", "default", 2000, "/img/icon/toast/error-icon.svg", "./login.html");
   }
 });
@@ -76,6 +76,22 @@ function cleanError(error) {
       return "Operation not allowed";
     case "auth/too-many-requests":
       return "Too many requests";
+    case "auth/invalid-credential":
+      return "Invalid credential";
+    case "auth/invalid-verification-code":
+      return "Invalid verification code";
+    case "auth/invalid-verification-id":
+      return "Invalid verification ID";
+    case "auth/missing-verification-code":
+      return "Missing verification code";
+    case "auth/missing-verification-id":
+      return "Missing verification ID";
+    case "auth/credential-already-in-use":
+      return "Credential already in use";
+    case "auth/missing-email":
+      return "Email is missing";
+    case "auth/missing-password":
+      return "Password is missing";
     default:
       return error.message.replace("Error ", "");
   }
@@ -145,10 +161,61 @@ $("[data-auth-role='logoutprompt']").click(function () {
 $(document.body).on("click", "[data-auth-role='logout'], .data-auth-logout", function () {
   signOut(auth);
 });
-
-$("[data-auth-role='to-reset-password']").click(function () {
-  window.location.href = "/dashboard/reset-password.html?reset-email=" + $('[data-auth-role="email-input"]').val();
+/**  PASSWORD RESET PAGES  **/
+$("[data-auth-role='to-forgot-password']").click(function () {
+  window.location.href = "/dashboard/forgot-password.html?reset-email=" + $('[data-auth-role="email-input"]').val();
+});
+$("[data-auth-role='forgot-password']").click(function () {
+  let email = $('[data-auth-role="email-input"]').val();
+  sendPasswordResetEmail(auth, email)
+    .then(() => {
+      // fill email into success page
+      $('[data-auth-role="reset-email"]').text(email);
+      // toggle to success contents
+      $('[data-auth-role="forgot-password-part"]').toggle();
+      new Toast("Password reset email sent", "default", 3000, "/img/icon/toast/success-icon.svg", "");
+    })
+    .catch((error) => {
+      new ErrorToast("Error sending password reset email", cleanError(error), 5000);
+    });
 });
 if (params.has("reset-email")) {
   $('[data-auth-role="email-input"], [data-auth-role="reset-src-email"]').val(params.get("reset-email"));
+}
+
+/**  CHOOSE NEW PASSWORD (reset.html)  **/
+
+if (window.location.pathname == "/dashboard/reset.html") {
+  if (params.has("oobCode")) {
+    let oobCode = params.get("oobCode");
+    verifyPasswordResetCode(auth, oobCode)
+      .then((email) => {
+        $('[data-auth-role="reset-email"]').text(email);
+        $('[data-auth-role="reset-password"]').click(function () {
+          //Must contain upper and lowercase letters, numbers, and symbols
+          let password = $('[data-auth-role="password-input"]').val(),
+            confirm_password = $('[data-auth-role="confirm-password-input"]').val();
+          if (password == confirm_password) {
+            if (password.match(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,}$/)) {
+              confirmPasswordReset(auth, oobCode, password)
+                .then(() => {
+                  window.location.href = "/dashboard/login.html";
+                })
+                .catch((error) => {
+                  new ErrorToast("Error resetting password", cleanError(error), 5000);
+                });
+            } else {
+              new ErrorToast("Error resetting password", "Password must be at least 8 characters, contain upper and lowercase letters, numbers, and symbols", 5000);
+            }
+          } else {
+            new ErrorToast("Error resetting password", "Passwords do not match", 5000);
+          }
+        });
+      })
+      .catch((error) => {
+        new ErrorToast("Error verifying password reset code", cleanError(error), 5000);
+      });
+  } else {
+    new Toast("Invalid password reset code. Returning to login", "default", 5000, "/img/icon/toast/warning-icon.svg", "/dashboard/login.html");
+  }
 }
