76e362b2399365c80e9f60c1f2844c73d314dd41
Sander Vonk <sander.c.vonk@gmail.com>
2023-01-08 21:31:17 -0800

    image loading tweaks (use newer formats)

--- js/util.js
+++ js/util.js
@@ -26,7 +26,7 @@ class Toast {
     overlay.classList.add("toast-overlay");
     toast.classList.add(this.type);
     if (this.icon != "") {
-      toast.innerHTML += `<img alt="icon" src="${this.icon}" class="toast-icon" alt="Toast Popup Icon">`;
+      toast.innerHTML += `<img loading="lazy" alt="icon" src="${this.icon}" class="toast-icon" alt="Toast Popup Icon">`;
     }
     toast.innerHTML += this.message;
     if (this.action != "") {
@@ -82,7 +82,7 @@ class Popup {
       $("<button></button>", { class: "popup-button " + (actionInfo[2] ? actionInfo[2] : ""), onclick: actionInfo[0], text: actionInfo[1] }).appendTo(buttons);
     }
     if (this.icon) {
-      $(`<img alt='Popup Icon' src="${this.icon}" class="popup-icon">`).appendTo(toast);
+      $(`<img loading="lazy" alt='Popup Icon' src="${this.icon}" class="popup-icon">`).appendTo(toast);
     }
     if (this.title) {
       $("<div></div>", { text: this.title, class: "popup-title" }).appendTo(toast);
